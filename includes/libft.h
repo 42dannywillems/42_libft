28b501f07b4b4cf87be0963561eb445e163f653f
Danny Willems <dan.wil@hotmail.com>
2014-08-31 14:29:53 +0200

    add count_t type and change/add prototypes

--- includes/libft.h
+++ includes/libft.h
@@ -3,17 +3,17 @@
 
 # include <stdlib.h>
 typedef unsigned int	size_t;
-typedef int				bool_t;
+typedef unsigned int	count_t;
 
 int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
-int		ft_isalnum(char c);
-int		ft_isalpha(char c);
-int		ft_isdigit(char c);
-int		ft_isechapcarac(char c);
-int		ft_islowercase(char c);
-int		ft_isuppercase(char c);
-int		ft_isprint(char c);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isechapcarac(int c);
+int		ft_islowercase(int c);
+int		ft_isuppercase(int c);
+int		ft_isprint(int c);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
@@ -30,5 +30,8 @@ char	*ft_strncat(char *dest, const char *src, size_t nb);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	ft_strncpy(const char *src, char *dest, size_t n);
 char	*ft_strstr(const char *str, const char *to_find);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*strchr(const char *s, int c);
 
 #endif
