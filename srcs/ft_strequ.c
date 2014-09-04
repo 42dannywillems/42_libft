d38a8fa099ee9615cd7be1ddb908866c5adc5461
Danny Willems <dan.wil@hotmail.com>
2014-09-04 13:29:14 +0200

    shortcut return value

--- srcs/ft_strequ.c
+++ srcs/ft_strequ.c
@@ -2,5 +2,5 @@
 
 bool_t		ft_strequ(char const *s1, char const *s2)
 {
-	return (ft_strcmp(s1, s2) == 0 ? TRUE : FALSE);
+	return (ft_strcmp(s1, s2) == 0);
 }
--- srcs/ft_strnequ.c
+++ srcs/ft_strnequ.c
@@ -2,5 +2,5 @@
 
 bool_t		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	return (ft_strncmp(s1, s2, n) == 0 ? TRUE : FALSE);
+	return (ft_strncmp(s1, s2, n) == 0);
 }
