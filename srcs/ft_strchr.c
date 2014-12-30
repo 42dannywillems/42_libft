0345e08f57af14a74c35e666f68fd2a3bcb54014
Danny Willems <dan.wil@hotmail.com>
2014-12-30 20:40:29 +0100

    If c not found, return the blank memory case of string argument

--- srcs/ft_strchr.c
+++ srcs/ft_strchr.c
@@ -1,14 +1,8 @@
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strchr(char *s, int c)
 {
-	if (*s)
-	{
-		if (*s == c)
-			return ((char *)s);
-		else
-			return (ft_strchr(s + 1, c));
-	}
-	else
-		return (NULL);
+	while (*s && *s != (unsigned char)c)
+		s++;
+	return (s);
 }
