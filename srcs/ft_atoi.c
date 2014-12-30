2ef56911dfef104df5dbd8a5605373c26864c84d
Danny Willems <dan.wil@hotmail.com>
2014-12-30 18:12:26 +0100

    gestion des cas positifs et négatifs. Regarder pour améliorer le code

--- srcs/ft_atoi.c
+++ srcs/ft_atoi.c
@@ -4,15 +4,24 @@ int		ft_atoi(const char *str)
 {
 	int		nbre;
 	count_t	i;
+	int		signe;
 
 	i = 0;
 	nbre = 0;
-	while (ft_isechapcarac(str[i]) || str[i] == '+')
+	signe = 1;
+	while (ft_isechapcarac(str[i]))
+		i++;
+	if (str[i] == '-')
+	{
+		signe = -1;
+		i++;
+	}
+	else if (str[i] == '+')
 		i++;
 	while (ft_isdigit(str[i]))
 	{
 		nbre *= 10;
-		nbre += str[i] - '0';
+		nbre += signe * (str[i] - '0');
 		i++;
 	}
 	return (nbre);
