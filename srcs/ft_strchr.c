990819e14926dae269a2c6ac86598ca0fb437629
Danny Willems <dan.wil@hotmail.com>
2014-12-30 21:39:26 +0100

    If not found and if null byte as second argument passed : OK. moulitest OK

--- srcs/ft_strchr.c
+++ srcs/ft_strchr.c
@@ -4,5 +4,7 @@ char	*ft_strchr(char *s, int c)
 {
 	while (*s && *s != (unsigned char)c)
 		s++;
-	return (s);
+	if (c == 0) // null bytes of s is considered
+		return (s);
+	return (*s ? s : NULL); // *s == 0 if not found --> NULL
 }
