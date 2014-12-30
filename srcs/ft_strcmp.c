c5e1b1beba1efb4d87d75978f2685b020f50a3b6
Danny Willems <dan.wil@hotmail.com>
2014-12-30 22:34:04 +0100

    Case two empty string

--- srcs/ft_strcmp.c
+++ srcs/ft_strcmp.c
@@ -2,7 +2,9 @@
 
 int		ft_strcmp(const char *s1, const char *s2)
 {
-	if (*s2 == 0) // Empty string
+	if (*s1 == 0 && *s2 == 0)
+		return (0);
+	else if (*s2 == 0) // Empty string
 		return (1);
 	while (*s1 && *s2 && *s1 == *s2)
 	{
