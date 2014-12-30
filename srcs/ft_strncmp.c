b7c464a814a2db2e82095510392535b344c5d1f2
Danny Willems <dan.wil@hotmail.com>
2014-12-30 20:52:17 +0100

    Empty case and n = 0 verified and OK

--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -5,7 +5,11 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	count_t i;
 
 	i = 1;
-	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2 && i < n)
+	if (*s2 == '\0') // Empty case
+		return (1);
+	else if (n == 0)
+		return (0);
+	while (*s1 && *s2 && *s1 == *s2 && i < n)
 	{
 		s1++;
 		s2++;
