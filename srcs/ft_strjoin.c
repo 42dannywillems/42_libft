2b437b9b3a2d31a95867239dc5a0151895d8619b
Danny Willems <dan.wil@hotmail.com>
2014-12-30 23:54:44 +0100

    The second call to strcpy musn't be fetched otherwise, we get the beginning of the second string

--- srcs/ft_strjoin.c
+++ srcs/ft_strjoin.c
@@ -6,12 +6,14 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	s_s2;
 	char	*n_s;
 
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
 	s_s1 = ft_strlen(s1);
 	s_s2 = ft_strlen(s2);
 	if ((n_s = ft_strnew(s_s1 + s_s2)))
 	{
 		n_s = ft_strcpy(n_s, s1);
-		n_s = ft_strcpy(n_s + s_s2, s2);
+		ft_strcpy(n_s + s_s2, s2);
 	}
 	return (n_s);
 }
