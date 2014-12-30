7b80c0b381505843e1b5a63f2d29f42cc61630d6
Danny Willems <dan.wil@hotmail.com>
2014-12-30 21:41:17 +0100

    First arg must be const

--- srcs/ft_strchr.c
+++ srcs/ft_strchr.c
@@ -1,10 +1,10 @@
 #include "libft.h"
 
-char	*ft_strchr(char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
 	while (*s && *s != (unsigned char)c)
 		s++;
 	if (c == 0) // null bytes of s is considered
-		return (s);
-	return (*s ? s : NULL); // *s == 0 if not found --> NULL
+		return ((char *)s);
+	return (*s ? (char *)s : NULL); // *s == 0 if not found --> NULL
 }
