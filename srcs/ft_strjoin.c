54f28f99adef526543f0c6973b1c847e9c7bbc09
Danny Willems <dan.wil@hotmail.com>
2014-12-31 00:58:21 +0100

    null checked

--- srcs/ft_strjoin.c
+++ srcs/ft_strjoin.c
@@ -6,8 +6,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	s_s2;
 	char	*n_s;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	ISNULL(s1);
+	ISNULL(s2);
 	s_s1 = ft_strlen(s1);
 	s_s2 = ft_strlen(s2);
 	if ((n_s = ft_strnew(s_s1 + s_s2)))
