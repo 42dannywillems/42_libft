2bb5eac16361773a10c1f5f95119fe211abf58b8
Danny Willems <dan.wil@hotmail.com>
2014-12-30 23:54:53 +0100

    empty case string

--- srcs/ft_strnequ.c
+++ srcs/ft_strnequ.c
@@ -2,5 +2,8 @@
 
 bool_t		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	return (ft_strncmp(s1, s2, n) == 0);
+	if (*s2)
+		return (ft_strncmp(s1, s2, n) == 0);
+	else // Empty string
+		return (TRUE);
 }
