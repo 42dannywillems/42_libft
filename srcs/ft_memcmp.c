282de132aba028a53eca636888e843c987243c13
Danny Willems <dan.wil@hotmail.com>
2015-01-01 21:36:45 +0100

    Check and return 0 if one argument is null

--- srcs/ft_memcmp.c
+++ srcs/ft_memcmp.c
@@ -5,6 +5,8 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n)
 	unsigned char *uc_s1;
 	unsigned char *uc_s2;
 
+	if (s1 == NULL || s2 == NULL)
+		return (0);
 	uc_s1 = (unsigned char *)s1;
 	uc_s2 = (unsigned char *)s2;
 	while (n-- > 0)
