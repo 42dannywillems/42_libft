9df5cde7afeca91d12b2d71ea6d66cdc48ad361d
Danny Willems <dan.wil@hotmail.com>
2015-01-06 13:00:44 +0100

    forgot to add 1 if not empty

--- srcs/gs_avltree_height.c
+++ srcs/gs_avltree_height.c
@@ -3,8 +3,8 @@
 size_t		gs_avltree_height(t_avltree *avltree)
 {
 	if (avltree)
-		return (MATH_MAX(	gs_avltree_height(avltree->left),
-							gs_avltree_height(avltree->right)));
+		return (1 + MATH_MAX(	gs_avltree_height(avltree->left),
+								gs_avltree_height(avltree->right)));
 	else
 		return (0);
 }
--- srcs/gs_bstree_height.c
+++ srcs/gs_bstree_height.c
@@ -3,8 +3,8 @@
 size_t		gs_bstree_height(const t_bstree *bstree)
 {
 	if (bstree)
-		return (MATH_MAX(	gs_bstree_height(bstree->left),
-							gs_bstree_height(bstree->right)));
+		return (1 + MATH_MAX(	gs_bstree_height(bstree->left),
+								gs_bstree_height(bstree->right)));
 	else
 		return (0);
 }
