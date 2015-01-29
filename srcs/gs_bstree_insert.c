cb26548f1a34cdb0c3afb7bd1a5d11eeabc71a66
Danny Willems <dan.wil@hotmail.com>
2015-01-29 15:10:17 +0100

    new algorithm. TO CHECK

--- srcs/gs_bstree_insert.c
+++ srcs/gs_bstree_insert.c
@@ -3,29 +3,40 @@
 t_bstree	*gs_bstree_insert(	t_bstree *bstree, void *data,
 								int cmp(void *, void *))
 {
-	t_bstree	*tmp;
+	t_bstree *tmp;
 
 	if (!gs_bstree_isempty(bstree))
 	{
-		if (cmp(bstree->data, data) < 0 && (tmp = gs_bstree_create(data)))
+		if (cmp(bstree->data, data) < 0)
 		{
 			if (bstree->right)
 				return (gs_bstree_insert(bstree->right, data, cmp));
-			bstree->right = tmp;
-			tmp->parent = bstree;
+			else if ((tmp = gs_bstree_create(data)))
+			{
+				bstree->right = tmp;
+				tmp->parent = bstree;
+				return (tmp);
+			}
+			else
+				return (NULL);
 		}
-		else if (cmp(bstree->data, data) > 0 && (tmp = gs_bstree_create(data)))
+		else if (cmp(bstree->data, data) > 0)
 		{
 			if (bstree->left)
 				return (gs_bstree_insert(bstree->left, data, cmp));
-			bstree->left = tmp;
-			tmp->parent = bstree;
+			else if ((tmp = gs_bstree_create(data)))
+			{
+				bstree->left = tmp;
+				tmp->parent = bstree;
+				return (tmp);
+			}
+			else
+				return (NULL);
 		}
-		else if (cmp(bstree->data, data) == 0)
-			return (bstree);
 		else
-			return (tmp);
+			return (bstree);
+		return (bstree);
 	}
-	bstree = gs_bstree_create(data);
-	return (bstree);
+	else
+		return (gs_bstree_create(data));
 }
