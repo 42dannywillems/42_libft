c5ff92bd7fadb26465eebae3080d753b078413b7
Danny Willems <dan.wil@hotmail.com>
2015-01-29 20:16:08 +0100

    join two strings and free the first

---
+++ srcs/ft_strjoin_free.c
@@ -0,0 +1,13 @@
+#include "libft.h"
+
+/* Same as strcat but realloc memory to be sure that s1 have enough space.
+ * Return pointer to the first character. */
+char	*ft_strjoin_free(char *s1, const char *s2)
+{
+	char *new;
+
+	new = ft_strjoin(s1, s2);
+//	ft_strdel(&s1);
+	
+	return (new);
+}
