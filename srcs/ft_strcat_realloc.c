517f83f643b670ceda61e4032343a6787d475f7a
Danny Willems <dan.wil@hotmail.com>
2015-01-29 20:14:33 +0100

    default message

--- srcs/ft_strcat_realloc.c
+++ srcs/ft_strcat_realloc.c
@@ -9,6 +9,6 @@ char	*ft_strcat_realloc(char *s1, const char *s2)
 	new = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
 	new = ft_strcat(new, s1);
 	new = ft_strcat(new, s2);
-	ft_strdel(&s1);
+	//ft_strdel(&s1);
 	return (new);
 }
--- srcs/ft_strndup.c
+++ srcs/ft_strndup.c
@@ -4,7 +4,7 @@ char	*ft_strndup(const char *src, size_t n)
 {
 	char *dest;
 
-	dest = ft_strnew(n);
+	dest = (char *)malloc(sizeof(char) * (n + 1));
 	if (dest)
 		dest = ft_strncpy(dest, src, n);
 	return (dest);
