ca73904b4152a132a7d8f3bfbc2ebe804ca8e06d
Danny Willems <dan.wil@hotmail.com>
2015-02-07 22:41:48 +0100

    pop methods implementation

--- includes/gs_stack.h
+++ includes/gs_stack.h
@@ -6,7 +6,7 @@
 # define gs_stack_clear(x)		gs_slist_clear(x)
 
 # define gs_stack_size(x)		gs_slist_size(x)
-// # define gs_stack_pop(x)		gs_slist_del_at(x, 0)
+# define gs_stack_pop(x)		gs_slist_pop(x)
 # define gs_stack_push(x, y)	gs_slist_push_front(x, y)
 # define gs_stack_merge(x, y)	gs_slist_merge(x, y)
 
