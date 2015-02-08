59c1950b7bc84276e2bf34f38663451f08277534
Danny Willems <dan.wil@hotmail.com>
2015-02-08 11:05:30 +0100

    haschild implementation. Doc OK

---
+++ srcs/gs_bstree_haschild.c
@@ -0,0 +1,24 @@
+#include "libft.h"
+
+/**
+ * \file gs_bstree_haschild.c
+ * \author Danny Willems
+ *
+ * \fn bool_t gs_bstree_haschild(t_bstree *bstree, int child)
+ * \brief Return true if bstree has a left child (resp. right) if child is set
+ * to left (resp. right).
+*/
+
+bool_t gs_bstree_haschild(t_bstree *bstree, int child)
+{
+	if (bstree)
+	{
+		if (child == LEFT)
+			return (gs_bstree_isempty(bstree->left) == FALSE);
+		else if (child == RIGHT)
+			return (gs_bstree_isempty(bstree->right) == FALSE);
+		else
+			return (FALSE);
+	}
+	return (FALSE);
+}
