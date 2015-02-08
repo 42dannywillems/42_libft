fffde88e173c1206e52da9ab56c4c8186f5bf9ab
Danny Willems <dan.wil@hotmail.com>
2015-02-08 14:41:44 +0100

    successor and predecessor implementation OK

--- srcs/gs_bstree_predecessor.c
+++ srcs/gs_bstree_predecessor.c
@@ -1,5 +1,12 @@
 /**
- * DO NOT USE. NOT TESTED AND ALGORITHM NOT CHECKED.
+ * \file gs_bstree_predecessor.c
+ * \author Danny Willems
+ *
+ * \fn t_bstree	*gs_bstree_predecessor(	t_bstree *bstree, void *data,
+										int cmp(void *, void *))
+	\brief Return the predecessor of data in bstree.
+
+	The predecessor is defined as the right-most value in the left subtree.
  */
 
 #include "libft.h"
@@ -9,9 +16,8 @@ t_bstree	*gs_bstree_predecessor(	t_bstree *bstree, void *data,
 {
 	t_bstree *tmp;
 	
-	tmp = gs_bstree_find(bstree, data, cmp);
-	if (tmp)
-		return (tmp->parent);
-	else
-		return (NULL);
+	if ((tmp = gs_bstree_find(bstree, data, cmp)))
+		return (gs_bstree_max(tmp->left));
+	return (NULL);
+
 }
