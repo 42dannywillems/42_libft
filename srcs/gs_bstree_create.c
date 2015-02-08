74e54ef7b9267e7fff31876c7e9cc34eb0fd6241
Danny Willems <dan.wil@hotmail.com>
2015-02-08 10:15:09 +0100

    documentation OK

--- srcs/gs_bstree_create.c
+++ srcs/gs_bstree_create.c
@@ -1,5 +1,10 @@
 /**
- * DO NOT USE. NOT TESTED AND ALGORITHM NOT CHECKED.
+ * \file gs_bstree_create.c
+ * \author Danny Willems
+ *
+ * \fn t_bstree *gs_bstree_create(void *data, t_bstree *parent, t_bstree
+ * *left, t_bstree *right)
+ * \brief Create a new bstree element.
  */
 
 #include "libft.h"
