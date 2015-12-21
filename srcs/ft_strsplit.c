50460fee430fd3a15a81e16bfe9107daaeb6e6bb
Danny Willems <dwillems@student.42.fr>
2015-12-21 23:20:45 +0100

    need to create a new string with ft_strnew of length 2 and assign the character to c to the first tab

--- srcs/ft_strsplit.c
+++ srcs/ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: dwillems <dwillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/19 14:03:09 by dwillems          #+#    #+#             */
-/*   Updated: 2015/12/19 14:03:18 by dwillems         ###   ########.fr       */
+/*   Updated: 2015/12/21 23:19:22 by dwillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 char	**ft_strsplit(const char *str, char c)
 {
-	return (ft_strsplit_str(str, &c));
+	char	*sep;
+
+	sep = ft_strnew(1);
+	sep[0] = c;
+	sep[1] = '\0';
+	return (ft_strsplit_str(str, sep));
 }
