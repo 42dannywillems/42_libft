68139a6b0adb560e9ff62ef2c2bbe34a353a7e17
Danny Willems <contact@danny-willems.be>
2015-12-21 20:02:27 +0100

    add print functions with color

---
+++ includes/gs_color.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gs_color.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dwillems <dwillems@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/21 18:29:44 by dwillems          #+#    #+#             */
+/*   Updated: 2015/12/21 18:30:43 by dwillems         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GS_COLOR_H
+# define GS_COLOR_H
+
+# define KNRM  "\x1B[0m"
+# define KRED  "\x1B[31m"
+# define KGRN  "\x1B[32m"
+# define KYEL  "\x1B[33m"
+# define KBLU  "\x1B[34m"
+# define KMAG  "\x1B[35m"
+# define KCYN  "\x1B[36m"
+# define KWHT  "\x1B[37m"
+
+#endif
