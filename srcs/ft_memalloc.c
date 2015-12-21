40afd14a3f73d1121f9887dda29aeb773b54ffc7
Danny Willems <contact@danny-willems.be>
2015-12-21 23:07:36 +0100

    case size = 0 added

--- srcs/ft_memalloc.c
+++ srcs/ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: dwillems <dwillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/19 12:12:19 by dwillems          #+#    #+#             */
-/*   Updated: 2015/12/19 12:12:22 by dwillems         ###   ########.fr       */
+/*   Updated: 2015/12/21 23:07:17 by dwillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_memalloc(size_t size)
 {
 	void *s;
 
+	if (size == 0)
+		return (NULL);
 	s = malloc(size);
 	s = ft_memset(s, 0, size);
 	return (s);
